e12c6b8ceafdb35a08db3087c679cbd276ad3054
Tryo Asnafi <tryoasnafi@gmail.com>
2022-03-25 09:48:43 +0700

    mengeksekusi properti dan fungsi kelas Greeter

--- src/main/kotlin/com/tryoasnafi/test_class/GreeterTest.kt
+++ src/main/kotlin/com/tryoasnafi/test_class/GreeterTest.kt
@@ -2,5 +2,10 @@ package com.tryoasnafi.test_class
 
 fun main() {
     val greeter = Greeter()
-    greeter.greet() // Hello world object
+    greeter.greet() // output: Hello world object
+    greeter.text = "Hi" // menetapkan nilai properti text menjadi "Hi"
+    greeter.greet("Anton") // output: Hi Anton
+    greeter.greet("Budi") // output: Hi Budi
+    greeter.text = "Hello Programmer" // menetapkan nilai properti text menjadi "Hello Programmer"
+    println(greeter.withRetVal("Dono")) // output: Hello Programmer Dono
 }
