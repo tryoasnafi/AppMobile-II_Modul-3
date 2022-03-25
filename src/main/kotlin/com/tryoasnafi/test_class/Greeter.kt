dea170bdc2a2d4a9559b1038b44ab929896bd0e0
Tryo Asnafi <tryoasnafi@gmail.com>
2022-03-25 09:46:35 +0700

    menambahkan properti text, fungsi greet(string), dan withRetVal(string)

--- src/main/kotlin/com/tryoasnafi/test_class/Greeter.kt
+++ src/main/kotlin/com/tryoasnafi/test_class/Greeter.kt
@@ -1,7 +1,19 @@
 package com.tryoasnafi.test_class
 
 class Greeter {
+    var text: String = "" // properti text untuk class Greeter
+
     fun greet() {
         println("Hello world object")
     }
+
+    // Overloading: membuat fungsi yang sama dengan paramater yang berbeda dan aksi yang berbeda.
+    fun greet(name: String) {
+        println("$text $name") // mencetak properti $text dan parameter $name
+    }
+
+    fun withRetVal(name: String): String { // name bertipe String dengan nilai kembalian berupa String juga
+        return "$text $name" // mengembalikan nilai properti $text dan parameter $name
+    }
+
 }
