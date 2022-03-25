e49ad7a6bb847920b86be1b40cc00cebeedf7ed0
Tryo Asnafi <tryoasnafi@gmail.com>
2022-03-25 15:32:01 +0700

    fix: typo percobaan inheritance

--- src/main/kotlin/com/tryoasnafi/test_class/12_InheritanceA.kt
+++ src/main/kotlin/com/tryoasnafi/test_class/12_InheritanceA.kt
@@ -3,7 +3,7 @@ package com.tryoasnafi.test_class
 // ubah visibility class parent menjadi open agar dapat digunakan
 open class Computer(val name: String, val brand: String) {}
 
-// extends pada kotlon menggunakan simbol colon atau titik dua (:)
+// extends pada kotlin menggunakan simbol colon atau titik dua (:)
 // variabel name dan brand dideklarasi di parent class
 class Laptop(name: String, brand: String, val batteryLife: Double) : Computer(name, brand) {
     fun info() {
